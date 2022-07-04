f497a0242f95ef9ec32b622d61bd1ad205d6141d
KAREN VARDANIAN <103530402+KarVarr@users.noreply.github.com>
2022-07-04 21:59:51 +0300

    2 add css and second challendge

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
-let React = require("react");
-let ReactDOM = require("react-dom");
+import React from "react";
+import ReactDOM from "react-dom";
+import "../public/styles.css";
 
 const name = `Karen`;
 const data = new Date().getFullYear();
 ReactDOM.render(
   <>
-    <h1>Created by {name}</h1>
-    <h2>Copyright {data}</h2>
+    <h1 className="heading">Created by {name}</h1>
+    <h2 spellCheck="false">Copyright {data}</h2>
+
+    <div className="divImg">
+      <img
+        alt="food"
+        src="https://www.myfoodtrip.com/wp-content/uploads/2020/05/Food-Blog-Directory.jpg"
+      />
+      <img
+        alt="food"
+        src="https://media.self.com/photos/622912847b959736301bfb91/2:1/w_2119,h_1059,c_limit/GettyImages-1301412050.jpg"
+      />
+      <img
+        alt="food"
+        src="https://images.immediate.co.uk/production/volatile/sites/30/2020/08/processed-food700-350-e6d0f0f.jpg?quality=90&resize=768,574"
+      />
+    </div>
   </>,
   document.getElementById("root")
 );
