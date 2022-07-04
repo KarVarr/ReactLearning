c02f7baee1936f382d56b5ca8a68626113407068
KAREN VARDANIAN <103530402+KarVarr@users.noreply.github.com>
2022-07-04 21:26:02 +0300

    1 add firs challenge

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
 let React = require("react");
 let ReactDOM = require("react-dom");
 
-ReactDOM.render(<h1>Hello World</h1>, document.getElementById("root"));
+const name = `Karen`;
+const data = new Date().getFullYear();
+ReactDOM.render(
+  <>
+    <h1>Created by {name}</h1>
+    <h2>Copyright {data}</h2>
+  </>,
+  document.getElementById("root")
+);
